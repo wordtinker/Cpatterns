097a61e9b9050433db1e7521b78756b3fde5330e
wordtinker <wordtinker@yandex.ru>
2017-02-03 21:36:11 +0300

    .

---
+++ Behavioural/CommandExample/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExample
+{
+    public class Radio
+    {
+        public const int MinVolume = 0;
+        public const int MaxVolume = 10;
+        public const int DefaultVolume = 5;
+
+        public Radio()
+        {
+            On = false;
+            Volume = DefaultVolume;
+        }
+        public virtual bool On { get; private set; }
+        public virtual int Volume { get; private set; }
+        
+        public virtual void SwitchOn()
+        {
+            On = true;
+            Console.WriteLine("Radio now on, volume level " + Volume);
+        }
+        public virtual void SwitchOff()
+        {
+            On = false;
+            Console.WriteLine("Radio now off");
+        }
+        public virtual void VolumeUp()
+        {
+            if (On)
+            {
+                if (Volume < MaxVolume)
+                {
+                    Volume++;
+                    Console.WriteLine("Volume turned up to level " + Volume);
+                }
+            }
+        }
+        public virtual void VolumeDown()
+        {
+            if (On)
+            {
+                if (Volume > MinVolume)
+                {
+                    Volume--;
+                    Console.WriteLine("Volume turned down to level " + Volume);
+                }
+            }
+        }
+    }
+    public class ElectricWindow
+    {
+        public ElectricWindow()
+        {
+            Open = false;
+            Console.WriteLine("Window is closed");
+        }
+        public virtual bool Open { get; private set; }
+        public virtual bool Closed
+        {
+            get
+            {
+                return (!Open);
+            }
+        }
+        public virtual void OpenWindow()
+        {
+            if (Closed)
+            {
+                Open = true;
+                Console.WriteLine("Window is now open");
+            }
+        }
+        public virtual void CloseWindow()
+        {
+            if (Open)
+            {
+                Open = false;
+                Console.WriteLine("Window is now closed");
+            }
+        }
+    }    public interface IVoiceCommand
+    {
+        void Execute();
+    }    public class VolumeUpCommand : IVoiceCommand
+    {
+        private Radio radio;
+        public VolumeUpCommand(Radio radio)
+        {
+            this.radio = radio;
+        }
+        public virtual void Execute()
+        {
+            radio.VolumeUp();
+        }
+    }    public class VolumeDownCommand : IVoiceCommand
+    {
+        private Radio radio;
+        public VolumeDownCommand(Radio radio)
+        {
+            this.radio = radio;
+        }
+        public virtual void Execute()
+        {
+            radio.VolumeDown();
+        }
+    }
+    public class WindowUpCommand : IVoiceCommand
+    {
+        private ElectricWindow window;
+        public WindowUpCommand(ElectricWindow window)
+        {
+            this.window = window;
+        }
+        public virtual void Execute()
+        {
+            window.CloseWindow();
+        }
+    }    public class WindowDownCommand : IVoiceCommand
+    {
+        private ElectricWindow window;
+        public WindowDownCommand(ElectricWindow window)
+        {
+            this.window = window;
+        }
+        public virtual void Execute()
+        {
+            window.OpenWindow();
+        }
+    }    public class SpeechRecogniser
+    {
+        private IVoiceCommand upCommand, downCommand;
+        public virtual void SetCommands(IVoiceCommand upCommand, IVoiceCommand downCommand)
+        {
+            this.upCommand = upCommand;
+            this.downCommand = downCommand;
+        }
+        public virtual void HearUpSpoken()
+        {
+            upCommand.Execute();
+        }
+        public virtual void HearDownSpoken()
+        {
+            downCommand.Execute();
+        }
+    }
+    /// <summary>
+    /// Commande decouples controller from the object it controls. 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a radio and its up/down command objects
+            Radio radio = new Radio();
+            radio.SwitchOn();
+            IVoiceCommand volumeUpCommand = new VolumeUpCommand(radio);
+            IVoiceCommand volumeDownCommand = new VolumeDownCommand(radio);
+            // Create an electric window and its up/down command objects
+            ElectricWindow window = new ElectricWindow();
+            IVoiceCommand windowUpCommand = new WindowUpCommand(window);
+            IVoiceCommand windowDownCommand = new WindowDownCommand(window);
+
+            // Create a speech recognition object
+            SpeechRecogniser speechRecogniser = new SpeechRecogniser();
+            // Control the radio
+            speechRecogniser.SetCommands(volumeUpCommand, volumeDownCommand);
+            Console.WriteLine("Speech recognition controlling the radio");
+            speechRecogniser.HearUpSpoken();
+            speechRecogniser.HearUpSpoken();
+            speechRecogniser.HearUpSpoken();
+            speechRecogniser.HearDownSpoken();
+            // Control the electric window
+            speechRecogniser.SetCommands(windowUpCommand, windowDownCommand);
+            Console.WriteLine("Speech recognition will now control the window");
+            speechRecogniser.HearDownSpoken();
+            speechRecogniser.HearUpSpoken();        }
+    }
+}
---
+++ Behavioural/MediatorExample/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorExample
+{
+    public class Ignition
+    {
+        private EngineManagementSystem mediator;
+        // Constructor accepts mediator as an argument
+        public Ignition(EngineManagementSystem mediator)
+        {
+            this.mediator = mediator;
+            On = false;
+            // Register back with the mediator;
+            mediator.RegisterIgnition(this);
+        }
+        public virtual void Start()
+        {
+            On = true;
+            mediator.IgnitionTurnedOn();
+            Console.WriteLine("Ignition turned on");
+        }
+        public virtual void Stop()
+        {
+            On = false;
+            mediator.IgnitionTurnedOff();
+            Console.WriteLine("Ignition turned off");
+        }
+        public virtual bool On { get; private set; }
+    }
+    public class Gearbox
+    {
+        private EngineManagementSystem mediator;
+        private bool enabled;
+        private Gear currentGear;
+        // Constructor accepts mediator as an argument
+        public Gearbox(EngineManagementSystem mediator)
+        {
+            this.mediator = mediator;
+            enabled = false;
+            currentGear = Gear.Neutral;
+            mediator.RegisterGearbox(this);
+        }
+        public virtual void Enable()
+        {
+            enabled = true;
+            mediator.GearboxEnabled();
+            Console.WriteLine("Gearbox enabled");
+        }
+        public virtual void Disable()
+        {
+            enabled = false;
+            mediator.GearboxDisabled();
+            Console.WriteLine("Gearbox disabled");
+        }
+        public virtual bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+        }
+        public virtual Gear Gear
+        {
+            set
+            {
+                if ((Enabled) && (Gear != value))
+                {
+                    currentGear = value;
+                    mediator.GearChanged();
+                    Console.WriteLine("Now in " + Gear + " gear");
+                }
+            }
+            get
+            {
+                return currentGear;
+            }
+        }
+    }
+
+    public enum Gear
+    {
+        Neutral,
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth,
+        Reverse
+    }
+
+    // MEdiator
+    public class EngineManagementSystem
+    {
+        private Ignition ignition;
+        private Gearbox gearbox;
+        private int currentSpeed;
+        public EngineManagementSystem()
+        {
+            currentSpeed = 0;
+        }
+        // Methods that enable registration with this mediator...
+        public virtual void RegisterIgnition(Ignition ignition)
+        {
+            this.ignition = ignition;
+        }
+        public virtual void RegisterGearbox(Gearbox gearbox)
+        {
+            this.gearbox = gearbox;
+        }
+        // Methods that handle object interactions...
+        public virtual void IgnitionTurnedOn()
+        {
+            gearbox.Enable();
+        }
+        public virtual void IgnitionTurnedOff()
+        {
+            gearbox.Disable();
+        }
+        public virtual void GearboxEnabled()
+        {
+            Console.WriteLine("EMS now controlling the gearbox");
+        }
+        public virtual void GearboxDisabled()
+        {
+            Console.WriteLine("EMS no longer controlling the gearbox");
+        }
+        public virtual void GearChanged()
+        {
+            Console.WriteLine("EMS disengaging revs while gear changing");
+        }
+        public virtual void AcceleratorEnabled()
+        {
+            Console.WriteLine("EMS now controlling the accelerator");
+        }
+        public virtual void AcceleratorDisabled()
+        {
+            Console.WriteLine("EMS no longer controlling the accelerator");
+        }
+        public virtual void BrakeEnabled()
+        {
+            Console.WriteLine("EMS now controlling the brake");
+        }
+        public virtual void BrakeDisabled()
+        {
+            Console.WriteLine("EMS no longer controlling the brake");
+        }
+        public virtual void BrakePressed()
+        {
+            currentSpeed = 0;
+        }
+        public virtual void BrakeReleased()
+        {
+            gearbox.Gear = Gear.First;
+        }
+    }
+
+    /// <summary>
+    /// The Mediator pattern helps to solve this through the definition of a separate class (the mediator) that
+    /// knows about the individual component classes and takes responsibility for managing their interaction.
+    /// The component classes also each know about the mediator class, but this is the only coupling they
+    /// have.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EngineManagementSystem mediator = new EngineManagementSystem();
+            Ignition ignition = new Ignition(mediator);
+            Gearbox gearbox = new Gearbox(mediator);
+            ignition.Start();
+            ignition.Stop();
+        }
+    }
+}
