1b11406b4902d9d78ccb355cb93e800496e267b0
wordtinker <wordtinker@yandex.ru>
2017-11-24 12:41:06 +0300

    Adaptive code book

---
+++ Assorted(Adaptive code)/NullObject/NullObject/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace NullObject
+{
+    public interface IUser
+    {
+        void IncrementSessionTicket();
+    }
+    public interface IUserRepository
+    {
+        IUser GetByID(Guid guid);
+    }
+
+    public class User : IUser
+    {
+        public void IncrementSessionTicket()
+        {
+            // STUB
+        }
+    }
+    public class NullUser : IUser
+    {
+        public void IncrementSessionTicket()
+        {
+            // DO nothing
+        }
+    }
+
+    class UserRepository : IUserRepository
+    {
+        public IUser GetByID(Guid guid)
+        {
+            //return null;
+            // 3.0
+            return new NullUser();
+        }
+    }
+    class Program
+    {
+        static IUserRepository userRepository = new UserRepository();
+
+        static void Main(string[] args)
+        {
+            var user = userRepository.GetByID(Guid.NewGuid());
+            // 1.0
+            try
+            {
+                // Without the Null Object pattern, this line could throw an exception
+                user.IncrementSessionTicket();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Yap, exception");
+            }
+            // 2.0
+            // client code should not check for null in any form
+            if (user != null)
+            {
+                user.IncrementSessionTicket();
+            }
+            // 2.1
+            user?.IncrementSessionTicket();
+            // 3.0 nothing is thrown as we expect NullUser object
+            user.IncrementSessionTicket();
+            // NB IsNull method is antipattern
+            // TODO See C#8.0 nullable reference type update
+        }
+    }
+}
---
+++ Assorted(Adaptive code)/SimpleDI/SimpleDI/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SimpleDI
+{
+    public interface ISecurityService
+    {
+        void ChangeUserPassword(string userID, string newPassword);
+    }
+    public interface IUserRepository
+    {
+        User GetByID(string userID);
+    }
+
+    public class SecurityService : ISecurityService
+    {
+        // 2.0 Add interface dependency
+        private readonly IUserRepository userRepository;
+        public SecurityService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository ?? throw new ArgumentNullException("userRepository");
+        }
+        public void ChangeUserPassword(string userID, string newPassword)
+        {
+            // 1.0 That code is not here initially
+            // 2.0 use DI
+            //UserRepository userRepository = new UserRepository();
+            User user = userRepository.GetByID(userID);
+            user.ChangePassword(newPassword);
+        }
+    }
+
+    public class User
+    {
+        internal void ChangePassword(string newPassword)
+        {
+            // STUB
+            throw new NotImplementedException();
+        }
+    }
+    public class UserRepository : IUserRepository
+    {
+        public User GetByID(string userID)
+        {
+            // STUB
+            throw new NotImplementedException();
+        }
+    }
+    public class AccountController
+    {
+        //private readonly SecurityService securityService;
+        // 2.0 depend on interface
+        private readonly ISecurityService securityService;
+        public AccountController(ISecurityService securityService)
+        {
+            // NB. 
+            // Dependent on concrete class
+            // Dependent on that class dependencies
+            // Impossible to test
+            // this.securityService = new SecurityService();
+            // 2.0 take it from injection
+            this.securityService = securityService ?? throw new ArgumentNullException("securityService");
+        }
+        public void ChangePassword(string userID, string newPassword)
+        {
+            // NB.
+            // Dependent on concrete class
+            // Dependent on that class dependencies
+            // Impossible to test
+            // UserRepository userRepository = new UserRepository();
+            // User user = userRepository.GetByID(userID);
+            // 1.0 Move implementation to sec.service
+            this.securityService.ChangeUserPassword(userID, newPassword);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new AccountController(new SecurityService(new UserRepository()));
+            // TODO
+            // who is creating that DI chain in MVVM?
+        }
+    }
+}
